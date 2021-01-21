98e288971ddcca6f55efab8e8bfa2c7e375dcde7
Deepshikha Gupta <deep.gupta.6391@gmail.com>
2021-01-20 23:09:05 -0500

    added signup page locatrs 2

--- src/main/java/com/qa/pages/actions/SignUpPage.java
+++ src/main/java/com/qa/pages/actions/SignUpPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 public class SignUpPage {
 	
 	By name=By.name("name");
+	By submit=By.name("submit");
 
 	public SignUpPage() {
 		System.out.println("this is signup page class");
